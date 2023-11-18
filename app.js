847383780216cafe41a29c07cc728dd92079e725
Max Ohsawa <max.ohsawa@gmail.com>
2023-11-18 18:23:31 -0500

    adds array typing example

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-{
-    var person = {
-        name: "max",
-        age: 36,
-    };
-    console.log(person.name);
+var person = {
+    name: "max",
+    age: 36,
+    hobbies: ["sports", "cooking"],
+};
+var favoriteActivities = [];
+favoriteActivities.push("four");
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
--- app.ts
+++ app.ts
@@ -1,6 +1,15 @@
 const person = {
   name: "max",
   age: 36,
+  hobbies: ["sports", "cooking"],
 };
 
+let favoriteActivities: string[] = [];
+
+favoriteActivities.push("four");
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
